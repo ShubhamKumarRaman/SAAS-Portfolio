dacafa8764a1ea69d8bab061e32f2f94e70e90d3
Shubham Kumar <shubham620409@gmail.com>
2026-05-10 19:18:42 +0530

    Create Project.js

---
+++ backend/src/models/Project.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const slugify = require('slugify')
+
+const projectSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        slug: {
+            type: String,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        techStack: [
+            {
+                type: String,
+            }
+        ],
+        githubLink: {
+            type: String,
+        },
+        liveLink: {
+            type: String
+        },
+        images: [
+            {
+                type: String
+            }
+        ],
+        featured: {
+            type: Boolean,
+            default: false,
+        },
+        tags: [
+            {
+                type: String
+            }
+        ]
+    }, { timestamps: true }
+)
+
+//Auto Generate Slug
+projectSchema.pre("save", function (next) {
+    if (!this.slug) {
+        this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+    next();
+})
+
+module.exports = mongoose.model("Project", projectSchema);
