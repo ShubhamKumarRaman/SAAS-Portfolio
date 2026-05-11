937aa7552c288fec4e388fd8e4be84b19f78335c
Shubham Kumar <shubham620409@gmail.com>
2026-05-11 19:19:21 +0530

    Create contactController.js

---
+++ backend/src/controllers/contactController.js
@@ -0,0 +1,118 @@
+const Message = require('../models/Message')
+
+const sendEmail = require('../services/emailService')
+
+//Create contact message
+const createContactMessage = async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
+
+        //Save message
+        const newMessage = await Message.create({ name, email, message });
+
+        //Send Email Notification
+        await sendEmail({
+            subject: "New Portfolio Contact",
+            sendTo: process.env.EMAIL_USER,
+            message: `
+            <strong>Name:</strong> ${name}<br/>
+            <strong>Email:</strong> ${email}<br/>
+            <strong>Message:</Strong><br/>
+            ${message}
+            `
+        })
+
+        res.status(201).json({
+            success: true,
+            message: "Message send successfully",
+            data: newMessage,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Get all Messages
+const getMessages = async (req, res) => {
+    try {
+        const message = await Message.find().sort({ createdAt: -1 })
+
+        res.json({
+            success: true,
+            count: message.length,
+            data: message
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Delete Message
+const deleteMessage = async (req, res) => {
+    try {
+        const message = await Message.findById(req.params.id);
+
+        if (!message) {
+            return res.status(400).json({
+                success: false,
+                message: "Message not found"
+            })
+        }
+
+        await message.deleteOne();
+
+        res.json({
+            success: true,
+            message: "Message deleted successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Mark message as read
+const markAsRead = async (req, res) => {
+    try {
+        const message = await Message.findByIdAndUpdate(
+            req.params.id,
+            {
+                status: "read"
+            },
+            { new: true }
+        );
+
+        if (!message) {
+            return res.status(404).json({
+                success: false,
+                message: "Message not found"
+            })
+        }
+
+        res.json({
+            success: true,
+            message: "Message marked as read",
+            data: message
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+module.exports = {
+    createContactMessage,
+    getMessages,
+    deleteMessage,
+    markAsRead,
+}
