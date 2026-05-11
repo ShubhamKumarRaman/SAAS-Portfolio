f09845d7c644b59567f1a71ce3bbee48f4aa4a01
Shubham Kumar <shubham620409@gmail.com>
2026-05-11 11:24:25 +0530

    Create Skill.js

---
+++ backend/src/models/Skill.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const skillSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        level: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 100,
+        },
+        icon: {
+            type: String
+        }
+    }, { timestamps: true }
+)
+
+module.exports = mongoose.model("SKill", skillSchema);
