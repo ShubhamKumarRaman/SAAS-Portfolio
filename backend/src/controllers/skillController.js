e761b23b7394ce447e49cdb3f908a2f8c022ac4a
Shubham Kumar <shubham620409@gmail.com>
2026-05-11 11:35:32 +0530

    Create skillController.js

---
+++ backend/src/controllers/skillController.js
@@ -0,0 +1,104 @@
+const Skill = require('../models/Skill')
+
+//Create Skill
+const createSkill = async (req, res) => {
+    try {
+        const skill = await Skill.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            messaged: "Skill created successfully",
+            data: skill
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Get all Skills
+const getSkills = async (req, res) => {
+    try {
+        const skills = await Skill.find().sort({
+            createdAt: -1
+        })
+
+        res.json({
+            success: true,
+            count: skills.length,
+            data: skills
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Update skill
+const updateSkill = async (req, res) => {
+    try {
+        const skill = await Skill.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+
+        if (!skill) {
+            res.status(404).json({
+                success: false,
+                message: "Skill not found"
+            })
+        }
+
+        res.json({
+            success: true,
+            message: "Skill updated",
+            data: skill,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Delete Skill
+const deleteSkill = async (req, res) => {
+    try {
+        const skill = await Skill.findById(req.params.id);
+
+        if (!skill) {
+            res.status(404).json({
+                success: false,
+                message: "Skill not found"
+            })
+        }
+
+        await skill.deleteOne();
+
+        res.json({
+            success: true,
+            message: "Skill Deleted"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+module.exports = {
+    createSkill, 
+    getSkills, 
+    updateSkill, 
+    deleteSkill,
+}
