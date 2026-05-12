50eb0ceff9167d0815e88face7d714cf65d03b97
Shubham Kumar <shubham620409@gmail.com>
2026-05-12 21:31:38 +0530

    Dashboard stats

--- backend/src/controllers/analyticsController.js
+++ backend/src/controllers/analyticsController.js
@@ -32,3 +32,46 @@ const trackVisitor = async (req, res) => {
         })
     }
 }
+
+//DashBoard stats
+const getDashboardStats = async (req, res) => {
+    try {
+        const totalProjects = await Project.countDocuments();
+
+        const totalBlogs = await Blog.countDocuments();
+
+        const totalSkills = await Skill.countDocuments();
+
+        const totalMessages = await Skill.countDocuments();
+
+        const totalVisitors = await Analytics.countDocuments();
+
+        const featuredProjects = await Project.countDocuments({
+            featured: true
+        });
+
+        const publishedBlogs = await Blog.countDocuments({ published: true });
+
+        const unreadMessages = await Message.countDocuments({ status: "unread" })
+
+        res.json({
+            success: true,
+            data: {
+                totalProjects,
+                totalBlogs,
+                totalSkills,
+                totalMessages,
+                totalVisitors,
+                totalVisitors,
+                featuredProjects,
+                publishedBlogs,
+                unreadMessages
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
