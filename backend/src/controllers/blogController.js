5156aba3070ba878324cd4e829b72492c473ded5
Shubham Kumar <shubham620409@gmail.com>
2026-05-12 10:38:31 +0530

    Get Blog

--- backend/src/controllers/blogController.js
+++ backend/src/controllers/blogController.js
@@ -17,3 +17,72 @@ const createBlog = async (req, res) => {
         })
     }
 }
+
+//Get all Blogs 
+const getBlogs = async (req, res) => {
+    try {
+        //Pagination
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 6;
+        const skip = (page - 1) * limit;
+
+        //Search
+        const keyword = req.query.search ?
+            {
+                title: {
+                    $regex: req.query.search,
+                    $options: "i"
+                }
+            }
+            : {};
+
+        //Publishd Filter
+        const publishFilter = req.query.published === "true"
+            ? { published: true }
+            : {};
+
+        // Tag Filter
+        const tagFilter = req.query.tagFilter
+            ? {
+                tags: {
+                    $in: [req.query.tag]
+                }
+            }
+            : {};
+
+        //Query
+        const blogs = await Blog.find({
+            ...keyword,
+            ...publishFilter,
+            ...tagFilter
+        })
+            .sort({
+                createdAt: -1,
+            })
+            .skip(skip)
+            .limit(limit);
+
+        //Count
+        const totalBlogs = await Blog.countDocuments({
+            ...keyword,
+            ...publishFilter,
+            ...tagFilter,
+        })
+
+        res.json({
+            success: true,
+            currentPage: page,
+            totalPages: Math.ceil(
+                totalBlogs / limit
+            ),
+            totalBlogs,
+            count: blogs.length,
+            data: blogs
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
