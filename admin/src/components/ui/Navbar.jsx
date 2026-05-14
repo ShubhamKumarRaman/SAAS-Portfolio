e1bad580a09804c5c51a0baa009731764a4dbc41
Shubham Kumar <shubham620409@gmail.com>
2026-05-14 09:17:19 +0530

    Create Navbar.jsx

---
+++ admin/src/components/ui/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>
+                Dashboard
+            </h2>
+
+            <div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
