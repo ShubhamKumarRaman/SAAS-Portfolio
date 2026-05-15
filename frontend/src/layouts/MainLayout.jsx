e5f582510c6b884e5fbbdf5d6f1479d7dca5fbb8
Shubham Kumar <shubham620409@gmail.com>
2026-05-15 21:22:50 +0530

    Create MainLayout.jsx

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom'
+
+import Navbar from '../components/layout/Navbar'
+import Footer from '../components/layout/Footer'
+
+const MainLayout = () => {
+  return (
+    <div className='min-h-screen bg-slate-950 text-white transition-all duration-300'>
+      <Navbar />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default MainLayout
