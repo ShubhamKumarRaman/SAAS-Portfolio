834aed5ea7d94930a590b4cc72a09c682bdd0a81
Shubham Kumar <shubham620409@gmail.com>
2026-05-16 21:45:22 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Toaster } from 'react-hot-toast'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App'
-import './styles/index.css'
+import App from "./App";
+import "./index.css";
 
-const queryClient = new QueryClient()
+import ThemeProvider from "./context/ThemeContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-
-        <Toaster
-          position="top-right"
-          reverseOrder={false}
-        />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
