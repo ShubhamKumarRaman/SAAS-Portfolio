ea969d7f4bcfbdd60cce95f0223c326672048f6e
Shubham Kumar <shubham620409@gmail.com>
2026-05-16 21:43:50 +0530

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,31 +1,55 @@
-import { Routes, Route } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import MainLayout from '../layouts/MainLayout'
+import MainLayout from "../layouts/MainLayout";
 
-import Home from '../pages/Home'
-import About from '../pages/About'
-import Projects from '../pages/Projects'
-import Skills from '../pages/Skills'
-import Blog from '../pages/Blog'
-import Resume from '../pages/Resume'
-import Contact from '../pages/Contact'
-import NotFound from '../pages/NotFound'
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Skills from "../pages/Skills";
+import Blog from "../pages/Blog";
+import Resume from "../pages/Resume";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='projects' element={<Projects />} />
-        <Route path='skills' element={<Skills />} />
-        <Route path='blog' element={<Blog />} />
-        <Route path='resume' element={<Resume />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='*' element={<NotFound />} />
-      </Route>
-    </Routes>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "skills",
+        element: <Skills />,
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+      {
+        path: "resume",
+        element: <Resume />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
-export default AppRoutes
+export default router;
