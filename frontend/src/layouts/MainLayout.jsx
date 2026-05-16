169487e79e5811b7fc7bd455709c51da291f74ee
Shubham Kumar <shubham620409@gmail.com>
2026-05-16 21:41:50 +0530

    Update MainLayout.jsx

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,20 +1,19 @@
-import { Outlet } from 'react-router-dom'
-
-import Navbar from '../components/layout/Navbar'
-import Footer from '../components/layout/Footer'
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className='min-h-screen bg-slate-950 text-white transition-all duration-300'>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
 
-      <main>
+      <main className="flex-1">
         <Outlet />
       </main>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
